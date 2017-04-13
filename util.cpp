b70db757f38d7a5114e1e83951abf457750c1816
TooSchoolForCool <zeyuz@outlook.com>
2017-04-14 00:05:20 +0800

    Add printToken function

---
+++ util.cpp
@@ -0,0 +1,31 @@
+#include "util.h"
+
+using namespace std;
+
+void printToken(Token *token)
+{
+	TokenType token_type = token->getTokenType();
+	
+	Integer *integer_token = (Integer *)token;
+	RealNumber *real_number_token = (RealNumber *)token;
+	// Word *word_token = (Word *)token;
+	Identifier *id_token = (Identifier *)token;
+
+	printf("line %d\t%-15s\t", token->getLineNumber(), TokenName[token_type].c_str());
+
+	switch(token_type)
+	{
+		case ID:
+			printf("%s\n", id_token->getID().c_str());
+			break;
+		case INT_NUMBER:
+			printf("%d\n", integer_token->getValue());
+			break;
+		case REAL_NUMBER:
+			printf("%lf\n", real_number_token->getValue());
+			break;
+		default:
+			printf("-\n");
+			break;
+	}
+}
---
+++ util.h
@@ -0,0 +1,19 @@
+/*********************************************************************
+ * MODULE: util.h
+ * PURPOSE: Some utility functions are implemented here
+ * AUTHER: Zeyu Zhang
+ * DATE STARTED: 2017-04-13
+ *********************************************************************/
+
+#ifndef UTIL_H
+#define UTIL_H
+
+#include "token.h"
+
+#include <iostream>
+
+#include <stdio.h>
+
+void printToken(Token *token);
+
+#endif	// UTIL_H
