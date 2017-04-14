3a40860e1981a3c1fd86489a32a5e1bbb84918d9
TooSchoolForCool <zeyuz@outlook.com>
2017-04-14 22:30:26 +0800

    Update main function

---
+++ main.cpp
@@ -0,0 +1,54 @@
+/*********************************************************************
+ * MODULE: main.cpp
+ * PURPOSE: main function laid here.
+ * AUTHER: Zeyu Zhang
+ * DATE STARTED:  2017-04-13
+ *********************************************************************/
+
+#include "lexer.h"
+#include "util.h"
+#include "globals.h"
+
+#include <iostream>
+#include <vector>
+
+#include <stdio.h>
+
+using namespace std;
+
+char src_file_path[1024] = "";
+char dst_file_path[1024] = "";
+
+bool TraceDestructor = false;
+bool TraceToken = false;
+
+CompilerMode mode = NOP;
+
+int main(int argc, char **argv)
+{
+	if(argc == 1)
+	{
+		usage();
+		return 0;
+	}
+
+	parseArgs(argc, argv);
+
+	switch(mode)
+	{
+		case NOP:
+			cout << "No operation" << endl;
+			break;
+		case LEX_ANALYSIS:
+			LexicalAnalysis(src_file_path, dst_file_path);
+			cout << "Lexical Analysis is done" << endl;
+			break;
+		default:
+			char msg[512];
+			sprintf(msg, "[main.cpp main()] no such option: %d", mode);
+			error(msg);
+			break;
+	}
+
+	return 0;
+}
